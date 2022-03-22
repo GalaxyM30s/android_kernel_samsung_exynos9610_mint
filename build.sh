8e534d4b89deb8a47586104edcae50a061e58265
John Vincent <git@tenseventyseven.cf>
2022-03-22 12:07:07 +0800

    build: Experimentally build the kernel with Rui-Clang
    Signed-off-by: John Vincent <git@tenseventyseven.cf>

--- build.sh
+++ build.sh
@@ -40,10 +40,10 @@ fi
 
 # Toolchain options
 BUILD_PREF_COMPILER='clang'
-BUILD_PREF_COMPILER_VERSION='proton'
+BUILD_PREF_COMPILER_VERSION='rui'
 
 # Local toolchain directory
-TOOLCHAIN=$HOME/toolchains/exynos9610_toolchains_fresh
+TOOLCHAIN=$HOME/toolchains/rui-clang
 
 # External toolchain directory
 TOOLCHAIN_EXT=$(pwd)/toolchain
@@ -65,7 +65,7 @@ exit_script() {
 }
 
 download_toolchain() {
-	git clone https://github.com/TenSeventy7/exynos9610_toolchains_fresh.git ${TOOLCHAIN_EXT} --single-branch -b ${BUILD_PREF_COMPILER_VERSION} --depth 1 2>&1 | sed 's/^/     /'
+	git clone https://git.project-ice.org/brickport-devs/rui-clang ${TOOLCHAIN_EXT} 2>&1 | sed 's/^/     /'
 	verify_toolchain
 }
 
